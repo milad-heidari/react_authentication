a3fe2acb5bcb253f4734cf99bd866d889a5ef93b
milad heidari <milad.heidari.dev@gmail.com>
2021-04-12 19:58:40 +0430

    import Home and Login routes to routes.js file

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,3 +1,12 @@
+import Home from '../Pages/Home'
+import Login from '../Pages/Login'
 export default [
-
+    {
+        path:'/',
+        component:Home
+    },
+    {
+        path:'login',
+        component:Login
+    }
 ]
