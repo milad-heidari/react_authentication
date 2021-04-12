f9b1fb87d49d161a05c76cee09b3d00e1dd00964
milad heidari <milad.heidari.dev@gmail.com>
2021-04-12 20:18:54 +0430

    import Switch and Route from react-router-dom and routes from ../src/Router/routes.js and use for handle router in this project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {Switch,Route} from 'react-router-dom'
+import routes from '../src/Router/routes'
 function App() {
   return (
     <div>
-      
+      <Switch>
+        {routes.map(route => <Route key='1' exact {...route}/>)}
+      </Switch>
     </div>
   )
 }
