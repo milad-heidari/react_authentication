c2977f7e85ba59df0b95bfc9fe047143c99a675c
milad heidari <milad.heidari.dev@gmail.com>
2021-04-12 20:15:54 +0430

    add commnts for routes.js

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -1,12 +1,19 @@
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
+
+
+/**
+ * 1-this router file for manage route of page.
+ * 2-in below array component key means page name:
+ *      for example: component:Home means Home.js in ../Pages/Login
+ */
 export default [
     {
         path:'/',
         component:Home
     },
     {
-        path:'login',
+        path:'/login',
         component:Login
     }
 ]
