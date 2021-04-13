0f28d8ffda2b4404a64cb653c1c291f04e08d523
milad heidari <milad.heidari.dev@gmail.com>
2021-04-13 12:27:34 +0430

    create Login DR and move to flie login.css and Login.js to this DR

---
+++ src/Authentication/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './login.css'
+function Login() {
+    return (
+        <div className='container'>
+            
+        </div>
+    )
+}
+
+export default Login
